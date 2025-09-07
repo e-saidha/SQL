ed6e5999a1b94eb2fb3595b2f00cfc1811ee7684
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-07 16:57:10 +0530

    Time: 1486 ms (79.54%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -5,7 +5,7 @@ UNION
 SELECT 'Average Salary' AS category, COUNT(*) AS accounts_count
 FROM Accounts
 WHERE income BETWEEN 20000 AND 50000
-UNION
+UNION 
 SELECT 'High Salary' AS category, COUNT(*) AS accounts_count
 FROM Accounts
 WHERE income > 50000
