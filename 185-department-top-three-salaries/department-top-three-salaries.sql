8b9fe988ea486a1d4189a48a4f9e6d531579f637
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-08 20:44:24 +0530

    Time: 848 ms (91.23%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
-WITH t1 AS (SELECT d.name AS Dep, e.name AS Ep, e.salary AS sal, DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS rk
+WITH t1 AS (SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary, DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS rk
 FROM Employee AS e
 JOIN Department AS d
 ON e.departmentId = d.id) 
 
 SELECT 
-    Dep AS Department,
-    Ep AS Employee,
-    sal AS Salary
+    Department,
+    Employee,
+    Salary
 FROM t1
 WHERE rk<4
 ORDER BY Department
