364a49396e2c44f007c76702269bff35e1f513f6
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-17 18:37:28 +0530

    Time: 337 ms (66.27%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,6 @@
-SELECT MIN(e2.name) AS name
-FROM Employee as e1
-JOIN Employee as e2 
-ON e1.managerId = e2.id 
+SELECT e2.name 
+FROM Employee e1
+JOIN Employee e2
+ON e1.managerId = e2.id
 GROUP BY e2.id
-HAVING COUNT(*)>=5
-
-
+HAVING COUNT(e2.id)>4
