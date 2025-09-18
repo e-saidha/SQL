7fa1ca4325b4760d5b9bd2332c4f57f61685527c
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-18 15:17:58 +0530

    Time: 216 ms (92.77%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,10 +1,5 @@
-
-
-SELECT * 
+SELECT *
 FROM Cinema
-WHERE id NOT IN (
-    SELECT id
-FROM Cinema
-WHERE description LIKE '%boring%'
-) AND MOD(id, 2) = 1
-ORDER BY rating DESC
+WHERE description NOT LIKE '%boring%'
+  AND MOD(id, 2) = 1
+ORDER BY rating DESC;
