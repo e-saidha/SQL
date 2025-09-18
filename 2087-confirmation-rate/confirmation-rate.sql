fc4edcbda3743045ed2b2e2504c38ea8096f8d68
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-18 15:08:51 +0530

    Time: 542 ms (99.75%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,8 +1,6 @@
-SELECT s.user_id, ROUND( COAlESCE(SUM(c.action = 'confirmed')/ COUNT(c.action), 0),2) AS confirmation_rate
-FROM Signups AS s
-LEFT JOIN Confirmations AS c
+SELECT s.user_id, COALESCE(ROUND(SUM(action = 'confirmed')/ COUNT(*),2),0.00) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
 ON s.user_id = c.user_id
 GROUP BY s.user_id
-
-
 
