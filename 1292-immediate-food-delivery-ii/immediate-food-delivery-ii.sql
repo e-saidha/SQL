dc43cb42f12b2a864a6e36bdb26ebd8590ec7b54
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-23 07:41:41 +0530

    Time: 599 ms (70.05%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,8 +1,9 @@
-SELECT 
-ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)*100
-/COUNT(DISTINCT customer_id),2) AS immediate_percentage
-FROM Delivery WHERE (customer_id, order_date) IN (
-SELECT customer_id, MIN(order_date)
+WITH t AS(
+SELECT *
+FROM Delivery 
+WHERE (customer_id,order_date) IN (SELECT customer_id,MIN(order_date) AS fd
 FROM Delivery
-GROUP BY customer_id
-)
+GROUP BY customer_id))
+
+SELECT ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)*100,2) AS immediate_percentage
+FROM t 
