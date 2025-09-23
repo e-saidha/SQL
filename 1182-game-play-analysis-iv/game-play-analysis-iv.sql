bcca2ee9fcfb8e0349348638e75b671c1037814b
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-23 16:08:20 +0530

    Time: 549 ms (63.76%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,22 +1,23 @@
-# Write your MySQL query statement below
-WITH first_login AS 
-(
-    SELECT player_id,
-    MIN(event_date) AS first
-    FROM Activity
-    GROUP BY player_id
-)
-, next_login AS
-(
-    SELECT player_id,
+WITH t AS 
+(SELECT
+    player_id,
     event_date,
-    LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS next
-    FROM Activity
-)
+    LEAD(event_date) OVER (PARTITION BY player_id ORDER BY event_date) AS nxtdate,
+    event_date + INTERVAL 1 DAY AS nxtday,
+    ROW_NUMBER() OVER (PARTITION BY player_id ORDER BY event_date) AS rn
 
-SELECT ROUND(SUM(CASE WHEN DATEDIFF(n.next,f.first) = 1 THEN 1 ELSE 0 END)
-/COUNT(DISTINCT f.player_id),2) AS fraction
-FROM first_login AS f
-JOIN next_login AS n
-ON f.player_id = n.player_id 
+FROM Activity)
+,t2 AS 
+(SELECT player_id
+FROM t
+WHERE rn = 1
+AND nxtdate = nxtday)
+
+SELECT ROUND(COUNT(*)/(SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction
+FROM t2
+
+
+
+
+
 
