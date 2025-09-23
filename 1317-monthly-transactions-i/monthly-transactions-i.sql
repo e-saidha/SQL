aaec5d1542b8a328b48b326c8168ebda7c9d49e8
EKOM P SAIDHA <141557121+e-saidha@users.noreply.github.com>
2025-09-23 07:11:55 +0530

    Time: 528 ms (92.86%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -2,6 +2,6 @@
 WITH t AS (SELECT id,country,state,amount,DATE_FORMAT(trans_date, '%Y-%m') AS month
 FROM Transactions)
 
-SELECT month, country, COUNT(*) AS trans_count, SUM(amount) AS trans_total_amount, SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count, SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+SELECT month, country, COUNT(*) AS trans_count, SUM(amount) AS trans_total_amount, SUM(state = 'approved') AS approved_count, SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM t
 GROUP BY country,month
