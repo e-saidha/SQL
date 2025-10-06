0d9cf2e56c3c807346442184e037e06e36ef058e
EKOM <141557121+e-saidha@users.noreply.github.com>
2025-10-06 11:09:45 +0530

    Time: 578 ms (43.20%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,12 +1,5 @@
-SELECT customer_id 
+
+SELECT customer_id
 FROM Customer
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) = (
-    SELECT COUNT(DISTINCT product_key)
-    FROM Product
-)
-
-
-
-
-
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(DISTINCT product_key) FROM Product )
