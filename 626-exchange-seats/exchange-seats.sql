f5b859013e3a2e25691951e0883c6de2017dab15
EKOM <141557121+e-saidha@users.noreply.github.com>
2025-12-17 13:56:39 +0530

    Time: 321 ms (70.31%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,8 +1,8 @@
-SELECT 
-CASE 
-    WHEN id = (SELECT MAX(id) FROM Seat) AND MOD(id,2) = 1 THEN id
-    WHEN MOD(id,2) = 1 THEN id+1
-    ELSE id-1 END AS id, student
-FROM Seat 
+WITH t AS (SELECT
+CASE WHEN id%2 = 0 THEN id-1 WHEN id%2 = 1 AND id NOT IN (SELECT MAX(id) FROM Seat) THEN id+1 ELSE id END AS id, student
+FROM Seat)
+
+SELECT id,student
+FROM t 
 ORDER BY id
 
