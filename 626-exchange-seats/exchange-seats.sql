cac672fe5d52811d8bae03de8be5f8b5b8a8e565
EKOM <141557121+e-saidha@users.noreply.github.com>
2025-12-17 14:02:51 +0530

    Time: 381 ms (12.69%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,8 +1,13 @@
-WITH t AS (SELECT
-CASE WHEN id%2 = 0 THEN id-1 WHEN id%2 = 1 AND id != (SELECT MAX(id) FROM Seat) THEN id+1 ELSE id END AS id, student
+WITH t AS (
+    SELECT MAX(id) as id
+    FROM Seat
+)
+
+,t1 AS (SELECT
+CASE WHEN id%2 = 0 THEN id-1 WHEN id%2 = 1 AND id != (SELECT id FROM t) THEN id+1 ELSE id END AS id, student
 FROM Seat)
 
 SELECT id,student
-FROM t 
+FROM t1 
 ORDER BY id
 
