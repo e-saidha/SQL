91f1ac2ab337dfa876c40ef532419fc268545b6e
EKOM <141557121+e-saidha@users.noreply.github.com>
2025-12-17 14:01:00 +0530

    Time: 329 ms (57.38%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,5 +1,5 @@
 WITH t AS (SELECT
-CASE WHEN id%2 = 0 THEN id-1 WHEN id%2 = 1 AND id NOT IN (SELECT MAX(id) FROM Seat) THEN id+1 ELSE id END AS id, student
+CASE WHEN id%2 = 0 THEN id-1 WHEN id%2 = 1 AND id != (SELECT MAX(id) FROM Seat) THEN id+1 ELSE id END AS id, student
 FROM Seat)
 
 SELECT id,student
