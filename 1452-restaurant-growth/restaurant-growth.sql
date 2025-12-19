db91e88a66cf90c1a7cbe86094caac4661276b1f
EKOM <141557121+e-saidha@users.noreply.github.com>
2025-12-19 22:39:41 +0530

    Time: 472 ms (7.32%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,13 +1,11 @@
-# Write your MySQL query statement below
-WITH sum_all AS (SELECT visited_on, SUM(amount) AS am 
-FROM Customer
-GROUP BY visited_on)
-
-,t2 AS ( SELECT visited_on, SUM(am) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount
-FROM sum_all 
-ORDER BY visited_on
-LIMIT 18446744073709551615 OFFSET 6
+WITH DailyTotals AS (
+    SELECT visited_on, SUM(amount) as amount
+    FROM Customer
+    GROUP BY visited_on
 )
-SELECT visited_on, amount, ROUND(amount/7,2) AS average_amount
-FROM t2
+SELECT visited_on,SUM(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+ROUND(AVG(amount) OVER (ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2) AS  average_amount
+FROM DailyTotals
 ORDER BY visited_on
+LIMIT 10000000
+OFFSET 6
