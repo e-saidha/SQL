fc2100104ece51861d082cc76dfa5449fe3db1e6
EKOM <141557121+e-saidha@users.noreply.github.com>
2025-12-22 13:03:58 +0530

    Time: 507 ms (73.44%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,17 +1,12 @@
-WITH t AS (SELECT pid
+WITH t AS 
+(SELECT
+    pid,
+    tiv_2016,
+    COUNT(*) OVER(PARTITION BY tiv_2015) AS tiv_2015_count,
+    COUNT(*) OVER(PARTITION BY lat,lon) AS location_count
 FROM Insurance
-WHERE tiv_2015 IN (
-SELECT tiv_2015
-FROM Insurance
-GROUP BY tiv_2015
-HAVING COUNT(*)>1)
-AND pid IN 
-(SELECT pid
-FROM Insurance
-GROUP BY lat,lon
-HAVING COUNT(*) = 1))
-
+)
 SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
-FROM Insurance
-WHERE pid IN 
-(SELECT pid FROM t)
+FROM t
+WHERE tiv_2015_count > 1
+AND location_count = 1
