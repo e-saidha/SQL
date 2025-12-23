98b5637b20b571a4a2ea22c2d6270389deb17762
EKOM <141557121+e-saidha@users.noreply.github.com>
2025-12-23 22:42:30 +0530

    Time: 720 ms (33.17%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,11 +1,8 @@
-WITH t AS (SELECT SUM(unit) AS sm, p.product_name
-FROM Orders AS o
-LEFT JOIN Products AS p
+
+SELECT p.product_name, SUM(unit) AS unit
+FROM Orders o
+LEFT JOIN Products p
 ON o.product_id = p.product_id
 WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
-GROUP BY o.product_id)
-
-
-SELECT product_name, sm AS unit
-FROM t
-WHERE sm>99
+GROUP BY o.product_id, p.product_name
+HAVING SUM(unit)>99
