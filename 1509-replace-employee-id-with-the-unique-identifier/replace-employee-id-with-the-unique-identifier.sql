22066d786937237550ccfac267ee3bfead8a84f4
EKOM <141557121+e-saidha@users.noreply.github.com>
2026-03-27 10:07:20 +0530

    Time: 1153 ms (52.09%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,4 @@
-SELECT e2.unique_id, e1.name
-FROM Employees AS e1
-LEFT JOIN EmployeeUNI AS e2
-ON e1.id = e2.id
+SELECT EmployeeUNI.unique_id, name
+FROM Employees 
+LEFT JOIN EmployeeUNI 
+ON Employees.id = EmployeeUNI.id
