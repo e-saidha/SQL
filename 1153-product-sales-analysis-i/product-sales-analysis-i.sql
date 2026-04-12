306bc55b1b90d49d334e2d3c1f06b70fce0c1244
EKOM <141557121+e-saidha@users.noreply.github.com>
2026-04-12 15:26:49 +0530

    Time: 1361 ms (23.53%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,9 @@
-SELECT p.product_name, s.year, s.price 
-FROM Sales s
-LEFT JOIN Product p
-ON s.product_id = p.product_id
+SELECT product_name, year, price
+
+FROM Sales AS s
+LEFT JOIN Product AS p ON
+
+s.product_id = p.product_id
+
+
+ 
